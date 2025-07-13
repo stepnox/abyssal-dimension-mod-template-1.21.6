012382cbdc858ae13401ef81b362d16451b61b34
stepnox <steppro49@gmail.com>
2025-07-13 18:46:51 +0200

    Comment to test starting project

--- src/main/java/net/stepnox/abyssalmod/AbyssalDimensionMod.java
+++ src/main/java/net/stepnox/abyssalmod/AbyssalDimensionMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// starting project
 public class AbyssalDimensionMod implements ModInitializer {
 	public static final String MOD_ID = "abyssal-dimension-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
